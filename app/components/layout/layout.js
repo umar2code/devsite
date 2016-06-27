93b7d8e02836009bcb0205d3ccc99382d5958233
pradeepgain <pgowrishetty@sysgain.com>
2016-06-27 17:39:48 +0530

    layout fixed

--- app/components/layout/layout.js
+++ app/components/layout/layout.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 angular.module('myApp.layout', ['ngRoute'])
 
 /* /!*.config(['$routeProvider', function($routeProvider) {
@@ -11,22 +12,39 @@ angular.module('myApp.layout', ['ngRoute'])
  }])*/
 
     .controller('View1Ctrl', [function($scope, element, attrs) {
+       //  var mainElem = document.getElementsByClassName("body");
+       //  var angularMainElem = angular.element(mainElem)[0];
+       //      console.log('Maiin Element', angularMainElem);
+       //  var angularMainWidth = angularMainElem.offsetWidth;
+       //  var angularMainHeight = angularMainElem.offsetHeight;
+       //  console.log('main Width is', angularMainWidth);
+       //  console.log('main Height is', angularMainHeight);
+       //  //var divElem = document.getElementById("panesContainer");
+       //  var divElem = document.getElementsByClassName("panesContainer");
+       //  console.log('divElem', divElem);
+       //  var angulaarDivElement = angular.element(divElem);
+       //  console.log('angulaarDivElement', angulaarDivElement);
+       //  angulaarDivElement.style = {
+       //      height:(angularMainHeight - 36)+'px',
+       //      width:(angularMainWidth)+'px'
+       //  };
+       //  console.log("layout element isss", element);
+       // // alert(angulaarDivElementWidth);
     }])
     .directive('master',function () {
-        function link(scope, element) {
-            scope.$watch(function(){
-                scope.style = {
-                    //height:(element[0].offsetHeight - 36)+'px'
-                    //width:(element[0].scrollWidth)+'px'
-                };
-            }
-
-            );
-
-
-        }
-        return {
-            restrict: 'AE',
-            link: link
-        };
+        // function link(scope, element) {
+        //     scope.$watch(function(){
+        //         scope.style = {
+        //             height:(element[0].offsetHeight - 36)+'px',
+        //             width:(element[0].scrollWidth)+'px'
+        //         };
+        //     });
+        //
+        //     ////console.log('pradeepepepep',element[0]);
+        //     //console.log('pradeepepepep',element[0].style.width);
+        // }
+        // return {
+        //     restrict: 'AE',
+        //     link: link
+        // };
     });
--- app/components/templates/templates.dir.js
+++ app/components/templates/templates.dir.js
@@ -1,6 +1,6 @@
+
 (function () {
     'use strict';
-
     angular
         .module('myApp')
         .controller('templates', templates);

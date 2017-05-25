2644d1fa56e05c20767e24e1ef81a9867156c0d8
umar <umar@sysgain.com>
2017-05-25 16:16:39 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ angular.module('myApp', [
     return {
 
         scrollRight :function (){
-        var children = $('#widgetsWrapper').children();d
+        var children = $('#widgetsWrapper').children();ddd
         var totalWidth = 0;d
         for (var i = 0; i < children.length; i++) {
             totalWidth += children[i].offsetWidth;d
